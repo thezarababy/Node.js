ba8cbded12b6dd9464530121a78cb02798195b60
thezarababy <jyokoduwa@gmail.com>
2025-08-04 15:33:42 +0100

    post and get

--- index.js
+++ index.js
@@ -1,38 +1,106 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const app = express();
+// app.use(express.json());
+// async function connectToBD() {
+//   try {
+//     await mongoose.connect("mongodb://localhost:27017/sailchat");
+//     console.log("database connected");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// connectToBD();
+// const UserSchema = new mongoose.Schema({
+//   firstName: String,
+//   lastName: String,
+//   password: String,
+//   email: String,
+//   isAdmin: Boolean,
+// });
+// const UserModel = new mongoose.model("User", UserSchema);
+
+// app.post("/signup", async (request, response) => {
+//   try {
+//     let newUser = new UserModel({
+//       firstName: request.body.firstName,
+//       lastName: request.body.lastName,
+//       password: request.body.password,
+//       email: request.body.email,
+//       isAdmin: request.body.isAdmin,
+//     });
+//     await newUser.save();
+//     response.json({
+//       message: "signup successfull",
+//       data: newUser,
+//     });
+//   } catch (err) {
+//     response.status(500).json({ error: "something went wrong" });
+//   }
+// });
+
+// app.get("/allUsers", async (req, res) => {
+//   try {
+//     const myUser = await UserModel.find();
+//     res.json({
+//       message: "all user gotten successfully",
+//       data: myUser,
+//     });
+//   } catch (err) {
+//     res.status(500).json({ error: "failed to fetch all users" });
+//   }
+// });
+
+// app.listen(8000, () => console.log("i am listening on this app"));
+
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
+const mongoose = require("mongoose");
 app.use(express.json());
-async function connectToDB() {
+
+const dBConnect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/databasename");
-    console.log("DB connected successfully");
+    await mongoose.connect("mongodb://localhost:27017/ajo");
+    console.log("i am connected");
   } catch (err) {
     console.log(err);
   }
-}
+};
+dBConnect();
+app.listen(9000, () => console.log("i am active"));
 
-const userSchema = new mongoose.Schema({
-  fistName: String,
-  lastName: String,
-  emailAddress: String,
+const AjoSchema = new mongoose.Schema({
+  username: String,
   password: String,
-  isAdmin: Boolean,
 });
-const userModel = mongoose.model("User", userSchema);
+const AjoModel = new mongoose.model("AjoUsers", AjoSchema);
 
-app.post("/", async (req, res) => {
-  res.send("this is the home route");
-});
+// create new user
 app.post("/signup", async (req, res) => {
-  let newUser = new userModel({
-    fistName: "joy",
-    lastName: "okoduwa",
-    emailAddress: "jyokoduwa@gmail.com",
-    password: "12345",
-    isAdmin: false,
-  });
-  newUser.save();
-  res.send("This is the sign up route");
+  try {
+    let newAjoUser = new AjoModel({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    await newAjoUser.save();
+    res.json({
+      message: "user created",
+      data: newAjoUser,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "failed to create user" });
+  }
 });
-connectToDB();
-app.listen(8000, () => console.log("user connected"));
+
+app.get("/allusers", async (req, res) => {
+  try {
+    const myAjoUser = await AjoModel.find();
+    res.json({
+      message: "all users gotten",
+      data: myAjoUser,
+    });
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).json({ error: "failed to fetch all users" });
+  }
+});
